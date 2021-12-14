f0f1a7408af1378bf116e881ee3268eafddc64c5
Thilo von Neumann <tvn@mail.upb.de>
2021-12-14 16:16:18 +0100

    Add a few options to the OverlapSampler

--- padercontrib/database/sms_librispeech_meeting/mixture_generator/meeting/overlap_sampler.py
+++ padercontrib/database/sms_librispeech_meeting/mixture_generator/meeting/overlap_sampler.py
@@ -1,6 +1,8 @@
+import warnings
 from dataclasses import dataclass
 import numpy as np
 from ..utils import collate_fn
+import padertorch as pt
 
 
 def _get_valid_overlap_region(examples, max_concurrent_spk, current_source):
@@ -35,7 +37,7 @@ def _get_valid_overlap_region(examples, max_concurrent_spk, current_source):
 
 
 @dataclass(frozen=True)
-class OverlapSampler:
+class OverlapSampler(pt.Configurable):
     max_concurrent_spk: int
 
     def __call__(self, examples, current_source, rng):
@@ -52,32 +54,70 @@ class OverlapSampler:
 
 @dataclass(frozen=True)
 class UniformOverlapSampler(OverlapSampler):
+    """
+    Attributes:
+        max_concurrent_spk: The maximum number of concurrently active speakers
+        p_silence: Approximate target value for silence probability after each
+            utterance. The resulting true probability is likely higher than
+            `p_silence` due to rejection sampling.
+        maximum_silence: The maximum amount of silence allowed between two
+            utterances, in samples
+        maximum_overlap: The maximum amount of overlap allowed between two
+            utterances, in samples
+        minimum_silence: The minimum amount of silence between two utterances
+            if silence is sampled, in samples
+        minimum_overlap: The minimum amount of overlap between two utterances
+            if overlap is sampled, in samples
+        margin: The minimum distance between two utterances that would violate
+            `max_concurrent_spk`. Can be used to account for the reverberation
+            tail
+    """
     p_silence: float
     maximum_silence: int
     maximum_overlap: int
+    minimum_silence: int = 0
+    minimum_overlap: int = 0
+    margin: int = 0
 
     def sample_offset(self, examples, maximum_overlap, rng):
-        def sample_shift():
-            if rng.uniform(0, 1) <= self.p_silence:
-                shift = self._sample_silence(rng)
+        # Sample the shift (relative to the latest speaker_end) with rejection
+        # sampling so that never more than max_concurrent_spk are active at the
+        # same time. This means that we see more silence than self.p_silence
+        if maximum_overlap <= 0:
+            # We can't sample overlap here, so sample silence
+            shift = self._sample_silence(rng)
+        else:
+            for _ in range(100):    # Arbitrary upper bound for num rejections
+                shift = self._sample_shift(rng)
+                if shift > -maximum_overlap + self.margin:
+                    break
             else:
-                shift = self._sample_overlap(rng)
-                shift = -1 * shift
-            return shift
+                # If the shift was rejected 100 times in a row, the region for
+                # overlap is probably too small. Fall back to sampling silence.
+                # This distorts the distribution slightly, but probably not
+                # notably.
+                warnings.warn(
+                    'Offset sampling failed 100 times in a row, sampling '
+                    'silence!'
+                )
+                shift = self._sample_silence(rng)
 
-        speaker_end = sorted(examples['speaker_end'])
-        shift = sample_shift()
-
-        while shift < -1 * maximum_overlap:
-            shift = sample_shift()
-
-        offset = sorted(speaker_end)[-1] + shift
+        # The final offset is global, but the shift is relative to the last
+        # speaker end time, so shift
+        offset = sorted(examples['speaker_end'])[-1] + shift
         return offset
 
+    def _sample_shift(self, rng):
+        if rng.uniform(0, 1) <= self.p_silence:
+            shift = self._sample_silence(rng)
+        else:
+            shift = -self._sample_overlap(rng)
+        return shift
+
     def _sample_silence(self, rng):
-        silence = rng.integers(0, self.maximum_silence)
+        silence = rng.integers(self.minimum_silence, self.maximum_silence)
         return silence
 
     def _sample_overlap(self, rng):
-        overlap = rng.integers(0, self.maximum_overlap)
+        overlap = rng.integers(self.minimum_overlap, self.maximum_overlap)
         return overlap
