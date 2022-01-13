7c25be64b01fccec353c0ac7574133e530deb395
Thilo von Neumann <tvn@mail.upb.de>
2022-01-13 09:26:44 +0100

    Remove sms_wsj dependencies

--- mms_msg/scenario.py
+++ mms_msg/scenario.py
@@ -5,8 +5,6 @@ from typing import List, Tuple
 import numpy as np
 import paderbox as pb
 from scipy.signal import fftconvolve
-from sms_wsj.database.utils import get_white_noise_for_signal
-from sms_wsj.reverb.reverb_utils import get_rir_start_sample
 
 from mms_msg import keys
 
@@ -353,6 +351,71 @@ def multi_channel_scenario_map_fn(
     audio_data[keys.OBSERVATION] = clean_mix
     add_microphone_noise(example, snr_range)
     return example
+
+
+def get_white_noise_for_signal(
+        time_signal,
+        *,
+        snr,
+        rng_state: np.random.RandomState = np.random
+):
+    """
+    Args:
+        time_signal:
+        snr: SNR or single speaker SNR.
+        rng_state: A random number generator object or np.random
+    """
+    noise_signal = rng_state.normal(size=time_signal.shape)
+
+    power_time_signal = np.mean(time_signal ** 2, keepdims=True)
+    power_noise_signal = np.mean(noise_signal ** 2, keepdims=True)
+    current_snr = 10 * np.log10(power_time_signal / power_noise_signal)
+
+    factor = 10 ** (-(snr - current_snr) / 20)
+
+    noise_signal *= factor
+    return noise_signal
+
+
+def get_rir_start_sample(h, level_ratio=1e-1):
+    """Finds start sample in a room impulse response.
+
+    Selects that index as start sample where the first time
+    a value larger than `level_ratio * max_abs_value`
+    occurs.
+
+    If you intend to use this heuristic, test it on simulated and real RIR
+    first. This heuristic is developed on MIRD database RIRs and on some
+    simulated RIRs but may not be appropriate for your database.
+
+    If you want to use it to shorten impulse responses, keep the initial part
+    of the room impulse response intact and just set the tail to zero.
+
+    Params:
+        h: Room impulse response with Shape (num_samples,)
+        level_ratio: Ratio between start value and max value.
+
+    >>> get_rir_start_sample(np.array([0, 0, 1, 0.5, 0.1]))
+    2
+    """
+    assert level_ratio < 1, level_ratio
+    if h.ndim > 1:
+        assert h.shape[0] < 20, h.shape
+        h = np.reshape(h, (-1, h.shape[-1]))
+        return np.min(
+            [get_rir_start_sample(h_, level_ratio=level_ratio) for h_ in h]
+        )
+
+    abs_h = np.abs(h)
+    max_index = np.argmax(abs_h)
+    max_abs_value = abs_h[max_index]
+    # +1 because python excludes the last value
+    larger_than_threshold = abs_h[:max_index + 1] > level_ratio * max_abs_value
+
+    # Finds first occurrence of max
+    rir_start_sample = np.argmax(larger_than_threshold)
+    return rir_start_sample
+
 
 
 def combine_speaker_signals(example):
