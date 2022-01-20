0bdf4162b1d6158dcaa5d02beb5fda0e93d9617d
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2022-01-20 14:26:52 +0100

    add get_rng_state

--- mms_msg/utils/utils.py
+++ mms_msg/utils/utils.py
@@ -12,6 +12,11 @@ def get_rng(*seed: [str, int]) -> np.random.Generator:
         '_'.join(map(str, seed)))
 
 
+def get_rng_state(*seed: [str, int]) -> np.random.RandomState:
+    return pb.utils.random_utils.str_to_random_state(
+        '_'.join(map(str, seed)))
+
+
 def get_rng_example(example, *seed) -> np.random.Generator:
     return get_rng(example['dataset'], example['example_id'], *seed)
 
