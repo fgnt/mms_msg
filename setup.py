c09dc67f35be6556bb78dce0a28f98eec9011978
Thilo von Neumann <tvn@mail.upb.de>
2022-01-25 15:06:19 +0100

    Install lazy_dataset from github

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'paderbox @ git+http://github.com/fgnt/paderbox',
         'padertorch @ git+http://github.com/fgnt/padertorch',
-        'lazy_dataset',
+        'lazy_dataset @ git+http://github.com/fgnt/lazy_dataset',
         'tqdm',
         'cached_property',
         'numpy',
