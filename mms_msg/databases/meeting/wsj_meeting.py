1409434f3c551adc6826d3a1d36961919f1c77c7
TCord <cord@nt.upb.de>
2022-05-11 14:03:24 +0200

    Add example classes for anechoic and reverberant WSJ meetings

--- mms_msg/databases/meeting/wsj_meeting.py
+++ mms_msg/databases/meeting/wsj_meeting.py
@@ -8,6 +8,91 @@ from mms_msg.sampling.environment.rir import RIRSampler
 from mms_msg.sampling.environment.scaling import UniformScalingSampler
 from mms_msg.databases.single_speaker.wsj.utils import filter_punctuation_pronunciation
 from mms_msg.sampling.pattern.meeting import MeetingSampler
+from mms_msg.sampling.pattern.meeting.overlap_sampler import UniformOverlapSampler
+from mms_msg.sampling.environment.noise import UniformSNRSampler
+from .database import AnechoicMeetingDatabase, ReverberantMeetingDatabase
+from paderbox.io import data_dir
+from ..reverberation.sms_wsj import SMSWSJRIRDatabase
+
+OVERLAP_SETTINGS = {
+    'no_ov': {
+        'max_concurrent_spk': 2,
+        'p_silence': 1,
+        'maximum_silence': 2 * 8000,
+        'maximum_overlap': 0,
+    },
+    'medium_ov': {
+        'max_concurrent_spk': 2,
+        'p_silence': 0.1,
+        'maximum_silence': 2 * 8000,
+        'maximum_overlap': 8 * 8000,
+    },
+    'high_ov': {
+        'max_concurrent_spk': 2,
+        'p_silence': 0.01,
+        'maximum_silence': 1 * 8000,
+        'maximum_overlap': 8 * 8000,
+        'hard_minimum_overlap': 2 * 8000,
+    }
+}
+
+
+def AnechoicWSJ8kHzMeeting(source_json_path=database_jsons / 'wsj_8k.json', duration=120 * 8000,
+                           overlap_conditions='medium_ov', num_speakers=(5, 6, 7, 8)):
+    """
+    Meetings based on the WSJ0-2mix dataset. The resulting mixtures will have a matching value range,
+    so that models trained on this data can be evaluated on WSJ0-2mix and vice versa.
+
+    Args:
+        num_speaker:
+        overlap_conditions:
+
+    Returns:
+
+    """
+    if isinstance(overlap_conditions, str):
+        try:
+            overlap_conditions = OVERLAP_SETTINGS[overlap_conditions]
+        except:
+            raise KeyError(f'No settings defined for overlap scenario {overlap_conditions}') from None
+
+    overlap_sampler = UniformOverlapSampler(**overlap_conditions)
+    meeting_sampler = MeetingSampler(duration, overlap_sampler)
+    return AnechoicMeetingDatabase(source_json_path=source_json_path,
+                                   num_speakers=num_speakers,
+                                   meeting_sampler=meeting_sampler,
+                                   scaling_sampler=UniformScalingSampler(5),
+                                   snr_sampler=UniformSNRSampler(20, 30))
+
+
+def ReverberantWSJ8kHzMeeting(source_json_path=database_jsons / 'wsj_8k.json', duration=120 * 8000,
+                              overlap_conditions='medium_ov', num_speakers=(5, 6, 7, 8),
+                              scenario_json_path=data_dir.db_dir / 'sms_wsj' / 'rirs' / 'scenarios.json'):
+    """
+    Meetings based on the WSJ0-2mix dataset. The resulting mixtures will have a matching value range,
+    so that models trained on this data can be evaluated on WSJ0-2mix and vice versa.
+
+    Args:
+        num_speaker:
+        overlap_conditions:
+
+    Returns:
+
+    """
+    if isinstance(overlap_conditions, str):
+        try:
+            overlap_conditions = OVERLAP_SETTINGS[overlap_conditions]
+        except:
+            raise KeyError(f'No settings defined for overlap scenario {overlap_conditions}') from None
+
+    overlap_sampler = UniformOverlapSampler(**overlap_conditions)
+    meeting_sampler = MeetingSampler(duration, overlap_sampler)
+    return ReverberantMeetingDatabase(source_json_path=source_json_path,
+                                      num_speakers=num_speakers,
+                                      meeting_sampler=meeting_sampler,
+                                      scaling_sampler=UniformScalingSampler(5),
+                                      snr_sampler=UniformSNRSampler(20, 30),
+                                      rir_database=SMSWSJRIRDatabase(scenario_json_path))
 
 
 class WSJ_8kHz_Meeting(JsonDatabase):
@@ -26,6 +111,7 @@ class WSJ_8kHz_Meeting(JsonDatabase):
     >>> ex['dataset'], ex['example_id'], sorted(set(ex['speaker_id']))
     ('cv_dev93_rng2357136044', '0_4k9c031d_4k2c0317_4k3c030p_4k8c0305', ['4k2', '4k3', '4k8', '4k9'])
     """
+
     def __init__(
             self,
             json_path: [str, Path] = database_jsons / 'wsj_8k.json',
