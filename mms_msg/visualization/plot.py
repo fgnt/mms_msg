5568e2adaaca770b3146587f298bd8325380105b
Thilo von Neumann <tvn@mail.upb.de>
2022-05-16 11:30:28 +0200

    Update notebooks

--- mms_msg/visualization/plot.py
+++ mms_msg/visualization/plot.py
@@ -1,13 +1,17 @@
+import contextlib
 from collections import defaultdict
 
 import paderbox as pb
 
 
-def plot_meeting(ex):
-    with pb.visualization.axes_context(columns=1, figure_size=(10, 3)) as ac:
-        speech_activity = defaultdict(pb.array.interval.zeros)
-        num_samples = pb.utils.nested.get_by_path(ex, 'num_samples.original_source', allow_early_stopping=True)
-        for o, l, s, in zip(ex['offset']['original_source'], num_samples, ex['speaker_id']):
-            speech_activity[s][o:o + l] = True
+def plot_mixture(ex, ax=None):
+    if ax is None:
+        from matplotlib import pyplot as plt
+        _, ax = plt.subplots(1, 1)
+    speech_activity = defaultdict(pb.array.interval.zeros)
+    num_samples = pb.utils.nested.get_by_path(ex, 'num_samples.original_source', allow_early_stopping=True)
+    for o, l, s, in zip(ex['offset']['original_source'], num_samples, ex['speaker_id']):
+        speech_activity[s][o:o + l] = True
 
-        pb.visualization.plot.activity(speech_activity, ax=ac.new)
+    pb.visualization.plot.activity(speech_activity, ax=ax)
+    ax.axvline(ex['num_samples']['observation'])
