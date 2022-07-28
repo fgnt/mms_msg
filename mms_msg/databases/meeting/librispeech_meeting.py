606525567d57a95690238a006267b33ca6a690e3
TCord <cord@nt.upb.de>
2022-07-28 09:47:10 +0200

    Add within chapter sampling to LibriSpeech meetings

--- mms_msg/databases/meeting/librispeech_meeting.py
+++ mms_msg/databases/meeting/librispeech_meeting.py
@@ -4,12 +4,14 @@ from mms_msg.sampling.environment.scaling import UniformScalingSampler
 from mms_msg.sampling.pattern.meeting import MeetingSampler
 from mms_msg.sampling.pattern.meeting.overlap_sampler import UniformOverlapSampler
 from mms_msg.sampling.environment.noise import UniformSNRSampler
+from mms_msg.databases.single_speaker.librispeech import LibriSpeech
 from .database import AnechoicMeetingDatabase
 from .wsj_meeting import OVERLAP_SETTINGS
 
 
 def AnechoicLibriSpeechMeeting(source_json_path=database_jsons / 'librispeech.json',
-                           duration=120 * 16000, overlap_conditions='medium_ov', num_speakers=(5, 6, 7, 8)):
+                               duration=120 * 16000, overlap_conditions='medium_ov', num_speakers=(5, 6, 7, 8),
+                               within_chapter=True):
     """
     Meetings based on the LibriSpeech corpus.
 
@@ -20,7 +22,8 @@ def AnechoicLibriSpeechMeeting(source_json_path=database_jsons / 'librispeech.js
         overlap_conditions: Specifies the overlap scenario, either via pre-defined scenarios or custom values
             either str or dict of overlap settings
         num_speakers: Number of speakers per meeting. Any permitted number of speakers needs to be listed.
-
+        within_chapter: If true, ensures that utterances of each speaker in a meeting are sampled from
+            the same chapter such that the acoustic properties (e.g. reciording device) don't differ
     Returns:
         Database object containing configurations for anechoic LibriSpeech meetings
     """
@@ -29,10 +32,14 @@ def AnechoicLibriSpeechMeeting(source_json_path=database_jsons / 'librispeech.js
             overlap_conditions = OVERLAP_SETTINGS[overlap_conditions]
         except:
             raise KeyError(f'No settings defined for overlap scenario {overlap_conditions}') from None
-
+    if within_chapter:
+        scenario_key = ('speaker_id', 'chapter_id')
+    else:
+        scenario_key = None
+    source_database = LibriSpeech(source_json_path, scenario_key=scenario_key)
     overlap_sampler = UniformOverlapSampler(**overlap_conditions)
     meeting_sampler = MeetingSampler(duration, overlap_sampler=overlap_sampler)
-    return AnechoicMeetingDatabase(source_database=JsonDatabase(source_json_path),
+    return AnechoicMeetingDatabase(source_database=source_database,
                                    num_speakers=num_speakers,
                                    meeting_sampler=meeting_sampler,
                                    scaling_sampler=UniformScalingSampler(5),
--- mms_msg/databases/single_speaker/librispeech/database.py
+++ mms_msg/databases/single_speaker/librispeech/database.py
@@ -5,12 +5,23 @@ from paderbox.io.data_dir import database_jsons
 
 
 class LibriSpeech(JsonDatabase):
-    def __init__(self, json_path=database_jsons / 'librispeech.json'):
+    def __init__(self, json_path=database_jsons / 'librispeech.json', scenario_key=None):
         super().__init__(json_path)
+        if isinstance(scenario_key, str):
+            scenario_key = (scenario_key,)
+        self.scenario_key = scenario_key
 
     def load_example(self, example):
         return load_audio(example, keys.OBSERVATION)
 
+    def add_scenario(self, example):
+        example['scenario'] = '_'.join([example[key] for key in sorted(self.scenario_key)])
+
+    def get_dataset(self, name=None):
+        dataset = super().get_dataset(name)
+        if self.scenario_key is not None:
+            dataset.map(self.add_scenario)
+        return dataset
 
 class LibriSpeech8kHz(LibriSpeech):
     def __init__(self, json_path=database_jsons / 'librispeech_8k.json'):
