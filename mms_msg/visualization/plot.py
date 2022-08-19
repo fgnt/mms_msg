3e6cdbd81705e67de2359566f27d656d8feb25da
Thilo von Neumann <tvn@mail.upb.de>
2022-08-19 09:15:01 +0200

    Support speech_activity in plot_mixture

--- mms_msg/visualization/plot.py
+++ mms_msg/visualization/plot.py
@@ -13,7 +13,24 @@ def plot_mixture(ex, sample_rate=None, ax=None):
     for o, l, s, in zip(ex['offset']['original_source'], num_samples, ex['speaker_id']):
         speech_activity[s][max(o, 0):o + l] = True
 
-    pb.visualization.plot.activity(speech_activity, ax=ax)
+    if 'speech_activity' in ex:
+        utterance_boundaries = speech_activity
+        speech_activity = defaultdict(pb.array.interval.zeros)
+        for o, l, a, s in zip(
+                ex['offset']['original_source'], num_samples,
+                ex['speech_activity'], ex['speaker_id']
+        ):
+            if o < 0:
+                a = a[-o:]
+            speech_activity[s][max(o, 0):o+l] |= a
+    else:
+        utterance_boundaries = None
+
+    pb.visualization.plot.activity(
+        speech_activity,
+        segment_boundary_intervals=utterance_boundaries,
+        ax=ax
+    )
     ax.axvline(ex['num_samples']['observation'])
     if sample_rate is not None:
         ax.set_xticks(ax.get_xticks())
